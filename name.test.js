a148138081d214b2be422616a6bdc959fc7ad36f
Artem Daniliants <artem@daniliants.com>
2022-11-17 13:13:50 +0200

    Initial commit

---
+++ name.test.js
@@ -0,0 +1,9 @@
+const hello = require('./name');
+
+test('Lets see if hello works', () => {
+    expect(hello('Mikko')).toBe('Hello Mikko');
+});
+
+test('Lets see if it works with int', () => {
+    expect(hello(4)).toBe('Hello 4');
+});
